b0c60ba675e84d8956554ef7f0e9906e895bc8cc
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-15 15:14:53 +0200

    Corrigir imports para Minecraft 1.21.1

--- src/client/java/com/nexus/derivative/core/NexusDerivativeStreaming.java
+++ src/client/java/com/nexus/derivative/core/NexusDerivativeStreaming.java
@@ -3,8 +3,13 @@ package com.nexus.derivative.core;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+
+import com.nexus.derivative.math.DerivativeState;
+import com.nexus.derivative.streaming.ChunkStreamingController;
 
 public class NexusDerivativeStreaming implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(this::onClientTick);
@@ -12,7 +17,9 @@ public class NexusDerivativeStreaming implements ClientModInitializer {
 
     private void onClientTick(MinecraftClient client) {
         if (client.player == null || client.world == null) return;
-        var player = client.player;
+
+        ClientPlayerEntity player = client.player;
+
         DerivativeState.update(player.getX(), player.getZ(), client.getLastFrameDuration());
         ChunkStreamingController.update(client.world, player);
     }
--- src/client/java/com/nexus/derivative/mixin/ClientChunkManagerMixin.java
+++ src/client/java/com/nexus/derivative/mixin/ClientChunkManagerMixin.java
@@ -1,18 +1,19 @@
 package com.nexus.derivative.mixin;
 
-import com.nexus.derivative.streaming.ChunkStreamingController;
 import net.minecraft.client.world.ClientChunkManager;
-import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.WorldChunk;
 import net.minecraft.world.chunk.ChunkStatus;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientChunkManager.class)
-public abstract class ClientChunkManagerMixin {
-    @Inject(method = "getChunk", at = @At("HEAD"), cancellable = true)
-    private void onGetChunk(int chunkX, int chunkZ, ChunkStatus leastStatus, boolean create, CallbackInfoReturnable<Chunk> cir) {
-        ChunkStreamingController.forceLoadChunk((Object)this, chunkX, chunkZ);
+public class ClientChunkManagerMixin {
+
+    @Inject(method = "getChunk", at = @At("HEAD"))
+    private void onGetChunk(int chunkX, int chunkZ, ChunkStatus status, boolean create, CallbackInfoReturnable<WorldChunk> cir) {
+        // TODO: hook de streaming
     }
 }
